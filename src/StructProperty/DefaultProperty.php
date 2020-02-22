605b934417f73726482cb77d8ef100dea21b9024
Neo Vg <neo@gothnet.eu>
2020-02-22 18:45:38 +0100

    ✨ Allow to unset properties

--- src/StructProperty/DefaultProperty.php
+++ src/StructProperty/DefaultProperty.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace NeoVg\Struct\StructProperty;
 
+use NeoVg\Struct\Helper\DebugHelper;
 use NeoVg\Struct\StructAbstract;
 
 /**
@@ -203,6 +204,30 @@ class DefaultProperty
     public function getValue()
     {
         return $this->_value;
+    }
+
+    /**
+     * @return $this
+     */
+    public function unsetValue()
+    {
+        if (!$this->isSet()) {
+            trigger_error(
+                sprintf(
+                    'Cannot unset already unset value of property %s::%s in %s',
+                    get_class($this->_parent),
+                    $this->_name,
+                    DebugHelper::getCaller()
+                ),
+                E_USER_ERROR
+            );
+        }
+
+        unset($this->_value);
+        $this->_isSet = false;
+        $this->_isDirty = false;
+
+        return $this;
     }
 
     /**
